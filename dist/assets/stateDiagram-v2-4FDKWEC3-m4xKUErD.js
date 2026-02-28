aeee5dbb35dd4c19f3816e2e6b27b98e97b06669
binbin <binbin@niubi.im>
2026-02-28 20:42:08 +0800

    Update dependencies and add new HTML structure. Upgrade KaTeX to version 0.16.33 and Mermaid to version 11.12.2. Introduce new index.html file for improved project structure and add new asset files for enhanced functionality.

---
+++ dist/assets/stateDiagram-v2-4FDKWEC3-m4xKUErD.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-s1CsAUNv.js";import{_ as i}from"./index-DS_F3MmX.js";import"./chunk-55IACEB6-DXB80Oof.js";import"./chunk-QN33PNHL-OHUWI4DP.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
